f9da544448f0b60f8783c7902ef3b80de21ca8e9
Lucas Rodrigo <93683684+LucasRodrigoADS@users.noreply.github.com>
2022-02-26 11:28:34 -0300

    Add files via upload

---
+++ Sessão 3 - Iniciando com Programação/dados_2.js
@@ -0,0 +1,24 @@
+let preco = 19.90;
+let desconto = 0.4;
+
+console.log(19.9 * 0.6);
+console.log(preco * (1 - desconto));
+
+
+let precoComDesconto = preco * (1 - desconto);
+console.log(precoComDesconto);
+
+let nome = "Caderno";
+let categoria = "Papelaria";
+console.log(nome + " " +categoria) // Contatenar
+console.log("Produto: " + nome 
+    + ", Categoria: " + categoria
+    + ", Preço: " + preco
+    + ", Desconto:  " + desconto
+    + " Valor final: " + precoComDesconto);
+
+
+// console.log("Produto: " + nome + ", Categoria: " + categoria+ ", Preço: " + preco+ ", Desconto:  " + desconto+ " Valor final: " + precoComDesconto);
+
+console.log(1 + 1); // soma
+console.log("1" + 1) // contatenação
---
+++ Sessão 3 - Iniciando com Programação/desafio_area.js
@@ -0,0 +1,10 @@
+// PI * raio * raio
+
+const raio = 10;
+const PI = 3.141592;
+let area = PI * raio * raio;
+
+console.log("A área da circunferência é:", + area + "m2.");
+
+area = Math.PI * raio * raio;
+console.log("A área da circunferência é:", + area + "m2.");
